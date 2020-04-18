30acdb23edd885877dc3c07d71c7625e32cbc486
Jeevan Pillay <jeevan.pillay8@gmail.com>
2020-04-18 11:43:49 +1000

    added structure and documentation for ipld example

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# ipld
+# IPLD Go Implementation Details
+This repository maintains methods that all the users to create multi-hash DAG's through the `ipfs-go-api` connector. Then, these multihashes are sent to IPFS system.
+
+## Outputs
+The out-of-the-box implementation doesn't require the user to install IPFS (or run a daemon), instead, it connects directly to Infura's Public IPFS Gateway.
+
+```
+sh = shell.NewShell("https://ipfs.infura.io:5001")
+```
+
+Change this to localhost if running locally.
+
+## Inputs
+The system stores inputs based on keyValueMap's, based on two parameters that the user specifies such as the `inputKey` and `inputValue`.
+```
+keyValueMap := make(map[string]interface{})
+```
+
+## Marshall
+The key-value pair then gets marshalled into JSON. This will be useful later!
+
+```
+entryJSON, err := json.Marshal(keyValueMap)
+```
+
+## Conversion
+The marshalled JSON then gets converted into it's CID, which, is actually a multihash construct of the key-value pair created from the input.
+```
+cid, err := sh.DagPut(entryJSON, "json", "cbor")
+
+```
+
+## Access Control
+Since, we have specified to use INFURA's Public Host (as specified above), we can now view the website that stores this beautiful multi-hash value.
+- [Tash Sultana](https://explore.ipld.io/#/explore/bafyreiarqq75yerg6zssp3sqtyrfeht4d4dxzzbnvtwiidsogg3oni5ali)
+- [Pink Floyd](https://explore.ipld.io/#/explore/bafyreifq5edjluhqczjinj23k3matyusyojiwrmk3hfekq6sth3wbejaiy)
+- [COVID-19](https://explore.ipld.io/#/explore/bafyreiebwgly5tutecxp3fohuwnz5z2c52jsww4pzjs76gcuxjbd3wsmbe)
--- ipld.go
+++ ipld.go
@@ -1,20 +1,79 @@
 package main
 
 import (
+	"bufio"
+	"encoding/json"
 	"fmt"
 	"os"
-	"strings"
 
 	shell "github.com/ipfs/go-ipfs-api"
 )
 
+// Global variable to handle all the IPFS API client calls
+var sh *shell.Shell
+
 func main() {
-	// Where your local node is running on localhost:5001
-	sh := shell.NewShell("localhost:5001")
-	cid, err := sh.Add(strings.NewReader("hi world!"))
+	// key-value pair
+	keyValueMap := make(map[string]interface{})
+
+	// localnode: infura (change for localhost)
+	sh = shell.NewShell("https://ipfs.infura.io:5001")
+
+	// print
+	fmt.Println("This client generates a dynamic key-value entry and stores it in IPFS!")
+
+	// scan
+	inputKey, inputValue := ScanInput()
+
+	// set
+	keyValueMap[inputKey] = inputValue
+
+	// Converting into JSON object
+	entryJSON, err := json.Marshal(keyValueMap)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// Display the marshaled JSON object before sending it to IPFS
+	jsonStr := string(entryJSON)
+	fmt.Println("The JSON object of your key-value entry is:")
+	fmt.Println(jsonStr)
+
+	// Dag PUT operation which will return the CID for futher access or pinning etc.
+	cid, err := sh.DagPut(entryJSON, "json", "cbor")
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error: %s", err)
 		os.Exit(1)
 	}
-	fmt.Printf("added %s", cid)
+	fmt.Println("------\nOUTPUT\n------")
+	fmt.Printf("WRITE: Successfully added %sHere's the IPLD Explorer link: https://explore.ipld.io/#/explore/%s \n", string(cid+"\n"), string(cid+"\n"))
+
+	// Fetch the details by reading the DAG for key "inputKey"
+	fmt.Printf("READ: Value for key \"%s\" is: ", inputKey)
+	res, err := GetDag(cid, inputKey)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(res)
+
+}
+
+// GetDag handles READ operations of a DAG entry by CID, returning the corresponding value
+func GetDag(ref, key string) (out interface{}, err error) {
+	err = sh.DagGet(ref+"/"+key, &out)
+	return
+}
+
+func ScanInput() (string, string) {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	fmt.Println("Enter value for the key field: ")
+	scanner.Scan()
+	inputKey := scanner.Text()
+
+	fmt.Println("Enter value for the value field: ")
+	scanner.Scan()
+	inputValue := scanner.Text()
+
+	return inputKey, inputValue
 }
